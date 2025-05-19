69534e5ac3f1199ca1ec83da6b4e7a9d1cf33e4c
dominic gut <dominic.gut@vgt.energy>
2025-05-19 11:52:39 +0200

    setup project

---
+++ frontend/android/app/src/main/java/energy/vgt/ucomconnectapp/MainActivity.java
@@ -0,0 +1,5 @@
+package energy.vgt.ucomconnectapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'energy.vgt.ucomconnectapp',
+  appName: 'frontend',
+  webDir: 'www'
+};
+
+export default config;
